e89640711c145b5627bbdfc131eb40d5ab8bcd35
Ron Hagafny <Ronhagafny20@gmail.com>
2017-05-27 22:37:27 +0300

    Changed name to Homie

--- dist/app.js
+++ dist/app.js
@@ -23273,7 +23273,6 @@ module.exports = getIteratorFn;
 
 
 (function (i, s, o, g, r, a, m) {
-    console.log('google analytics');
     i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments);
     }, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
