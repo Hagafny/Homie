07c91d70b039fdc76f13621c30aa594d1ba4f2a3
Ron Hagafny <Ronhagafny20@gmail.com>
2017-05-28 12:15:39 +0300

    Changing recitation and lecture icons depending on where they come from

--- client/Components/Resources.jsx
+++ client/Components/Resources.jsx
@@ -12,15 +12,18 @@ export default class Resources extends React.Component {
         super(props)
     }
 
+    checkImage(imageUrl) {
+        return imageUrl.includes("moodle")? moodle : gdrive
+    }
     render() {
 
         return (
             <ul className="resourceList">
-                <li style={{listStyleImage: pdf}} > <Resource url={this.props.data.homework} img={pdf} >HW</Resource> </li>
-                <li>  <Resource url={this.props.data.moodle} img={moodle}>Submit</Resource></li>
-                <li> <Resource url={this.props.data.lecture} img={gdrive}>Lecture</Resource></li>
-                <li>  <Resource url={this.props.data.recitation} img={gdrive} >Recitation</Resource></li>
-                <li> <Resource prefix={"https://piazza.com/class/"} url={this.props.data.piazza} img={piazza}>Piazza</Resource></li>
+                <li><Resource url={this.props.data.homework} img={pdf} >HW</Resource> </li>
+                <li><Resource url={this.props.data.moodle} img={moodle}>Submit</Resource></li>
+                <li><Resource url={this.props.data.lecture} img={this.checkImage(this.props.data.lecture)}>Lecture</Resource></li>
+                <li><Resource url={this.props.data.recitation} img={this.checkImage(this.props.data.recitation)}>Recitation</Resource></li>
+                <li><Resource prefix={"https://piazza.com/class/"} url={this.props.data.piazza} img={piazza}>Piazza</Resource></li>
             </ul>
         )
     };
