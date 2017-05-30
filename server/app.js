119ed87c705970b24cdf63dfa41d34c03b5d82d6
Ron Hagafny <Ronhagafny20@gmail.com>
2017-05-31 01:24:39 +0300

    merging dev with master

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../dist'))); 
 app.use(favicon(path.join(__dirname + './../client/images/favicon.ico')));
 
 app.get('/api/assignment/', function (req, res) {
