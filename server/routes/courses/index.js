f2a6ee88ca90a0d5c85dba00474aa46b67402c41
Ron Hagafny <Ronhagafny20@gmail.com>
2017-10-28 22:01:26 +0300

    Removed auth check from courses basic cause we need them on the main page as well. This should solve mobile bug

--- server/routes/courses/index.js
+++ server/routes/courses/index.js
@@ -3,9 +3,10 @@ const courses = router();
 const handlers = require('./handlers');
 const authMiddleware = require('../../authMiddleware.js');
 
+courses.get('/basic/:ids', handlers.getCoursesBasic);
+
 courses.use(authMiddleware);
 
-courses.get('/basic/:ids', handlers.getCoursesBasic);
 courses.get('/:ids', handlers.getCourses);
 courses.post('/', handlers.save);
 courses.put('/', handlers.edit);
