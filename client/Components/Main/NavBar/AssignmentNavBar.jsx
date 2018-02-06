b7e91ea0db6c72dde76f8cc1ef3cc6138007e8d0
Hagafny <ronhagafny20@gmail.com>
2018-02-07 01:24:14 +0200

    added links to homepage from the logo and Homie text

--- client/Components/Main/NavBar/AssignmentNavBar.jsx
+++ client/Components/Main/NavBar/AssignmentNavBar.jsx
@@ -4,6 +4,7 @@ import OptionsNavBarTab from './OptionsNavBarTab.jsx';
 import Logo from '../../../images/HomieLogo.png'; // Homie Logo
 import StudentUnionLogo from '../../../images/StudentUnion.png'; // Student Union Logo
 const AssignmentNavBar = (props) => {
+    const homePageUrl = 'https://tryhomieapp.herokuapp.com/';
 
     return <div className="container">
         <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
@@ -11,9 +12,9 @@ const AssignmentNavBar = (props) => {
                 <span className="navbar-toggler-icon"></span>
             </button>
             <span className="navbar-brand" >
-                <img src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+                <a className="noStyle" href={homePageUrl} target="_blank"><img src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" /> </a>
                 <img src={StudentUnionLogo} width="30" height="30" className="d-inline-block align-top" alt="" />
-                Homie
+                <a className="noStyle" href={homePageUrl} target="_blank">Homie</a>
             </span>
 
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
